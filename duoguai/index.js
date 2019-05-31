5b5a8c5a110db060c29b23000fda40f3b94b7540
MyEyouGame <45836764+MyEyouGame@users.noreply.github.com>
2019-05-31 14:18:35 +0800

    啊

--- duoguai/index.js
+++ duoguai/index.js
@@ -115,52 +115,17 @@
 
 	function startGame() {
 		CAnimation();
-		
-		//获取元素
-		var dv = document.getElementById('character');
-		var x = 0;
-		var y = 0;
-		var l = 0;
-		var t = 0;
-		var isDown = false;
-		//鼠标按下事件
-		dv.onmousedown = function(e) {
-			//获取x坐标和y坐标
-			x = e.clientX;
-			y = e.clientY;
-
-			//获取左部和顶部的偏移量
-			l = dv.offsetLeft;
-			t = dv.offsetTop;
-			//开关打开
-			isDown = true;
-			//设置样式  
-			dv.style.cursor = 'move';
-		}
-		//鼠标移动
-		window.onmousemove = function(e) {
-			if (isDown == false) {
-				return;
-			}
-			//获取x和y
-			var nx = e.clientX;
-			var ny = e.clientY;
-			//计算移动后的左偏移量和顶部的偏移量
-			var nl = nx - (x - l);
-			var nt = ny - (y - t);
-
-			dv.style.left = nl + 'px';
-			dv.style.top = nt + 'px';
-		}
-		//鼠标抬起事件
-		dv.onmouseup = function() {
-			//开关关闭
-			isDown = false;
-			dv.style.cursor = 'default';
-		}
-		
+	 
 	}
 	
+	//碰撞检测
+	
+	
+	//获取两个矩形相交区域
+    function getInRect(x1,y1,x2,y2,x3,y3,x4,y4) {
+        return [Math.max(x1,x3),Math.max(y1,y3),Math.min(x2,x4),Math.min(y2,y4)];
+    }
+ 
     function lose1(){
 		loseContainer.style.display = "block";
 		character.style.zIndex = "0";
