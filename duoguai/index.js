676d567f18636c1c6845963781ee159e8aa1be30
MyEyouGame <45836764+MyEyouGame@users.noreply.github.com>
2019-05-31 13:35:58 +0800

    Update index.js

--- duoguai/index.js
+++ duoguai/index.js
@@ -110,31 +110,57 @@
 			// var oEvent = oEvent||event;  
 	    // }
 
-     var dragCircle = document.getElementById('character');
  
-	dragCircle.onmousedown = function(ev) {
-        var ev = ev || window.event; 
-
-        var relaX = ev.clientX - this.offsetLeft;
-        var relaY = ev.clientY - this.offsetTop;
-
-	// 获取当前鼠标位置，减去与div的相对位置得到当前div应该被拖拽的位置
-        document.onmousemove = function(ev) {
-            var ev = ev || window.event;
-            dragCircle.style.left = ev.clientX - relaX + 'px';
-            dragCircle.style.top = ev.clientY - relaY + 'px';
-        };
-        document.onmouseup = function(ev) {
-          var ev = ev || window.event;
-          document.onmousemove = null;
-          document.onmouseup = null;
-         }
-     }
-
  
 
 	function startGame() {
 		CAnimation();
+		
+			//获取元素
+	var dv = document.getElementById('character');
+	var x = 0;
+	var y = 0;
+	var l = 0;
+	var t = 0;
+	var isDown = false;
+	//鼠标按下事件
+	dv.onmousedown = function(e) {
+		//获取x坐标和y坐标
+		x = e.clientX;
+		y = e.clientY;
+
+		//获取左部和顶部的偏移量
+		l = dv.offsetLeft;
+		t = dv.offsetTop;
+	
+		//开关打开
+		isDown = true;
+		//设置样式  
+		dv.style.cursor = 'move';
+		
+		
+	}
+		//鼠标移动
+		window.onmousemove = function(e) {
+			if (isDown == false) {
+				return;
+			}
+			//获取x和y
+			var nx = e.clientX;
+			var ny = e.clientY;
+			//计算移动后的左偏移量和顶部的偏移量
+			var nl = nx - (x - l);
+			var nt = ny - (y - t);
+	
+			dv.style.left = nl + 'px';
+			dv.style.top = nt + 'px';
+		}
+		//鼠标抬起事件
+		dv.onmouseup = function() {
+			//开关关闭
+			isDown = false;
+			dv.style.cursor = 'default';
+		}
 	}
 	
     function lose1(){
